1ab77f0b623ec3b1aa00bf243c5a80ce1682b7a6
arvindrajcs <arvindrajcs@gmail.com>
2025-03-04 15:31:30 +0530

    api config.js

---
+++ src/config/apiConfig.js
@@ -0,0 +1,10 @@
+
+
+// export const API_HOST = 'http://103.133.215.182:8085';
+export const API_HOST = 'http://localhost:8080';
+
+export const DEPARTMENT ="/department";
+export const DOCTOR ="/doctor";
+export const SESSION = "/session";
+export const APPOINTMENT = "/appointment";
+
