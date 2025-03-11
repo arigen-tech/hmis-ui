9d3b105f8c85de9aaa5dd45ca15f713efb28fb8b
arvindrajcs <arvindrajcs@gmail.com>
2025-03-11 14:50:41 +0530

    TEST EMP REGISTER

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
-// export const API_HOST = 'http://103.133.215.182:8085';
-export const API_HOST = 'http://localhost:8080';
+export const API_HOST = 'http://103.133.215.182:8445';
+// export const API_HOST = 'http://localhost:8080';
 // export const API_HOST = 'http://192.168.35.20:8080';
 
 
@@ -10,3 +10,9 @@ export const APPOINTMENT = "/app";
 
 export const LOGIN = "/authController/login";
 
+export const COUNTRYAPI = "/country";
+export const STATEAPI = "/state";
+export const DISTRICTAPI = "/district";
+export const GENDERAPI = "/gender";
+
+export const EMPLOYEE = "/employees";
