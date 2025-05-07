8bb14e0b65287aec193773b05a0f19168802b57e
“HariomBhardwaj19” <hbstudent1997@gmail.com>
2025-05-07 15:40:12 +0530

    relation,gender,blood,gender resp.

--- src/Views/Masters/MaritalStatusMaster/index.js
+++ src/Views/Masters/MaritalStatusMaster/index.js
@@ -274,7 +274,7 @@ const renderPagination = () => {
                       </button>
                       <button
                         type="button"
-                        className="btn btn-success me-2"
+                        className="btn btn-success me-2 flex-shrink-0"
                         onClick={handleRefresh}
                       >
                         <i className="mdi mdi-refresh"></i> Show All
