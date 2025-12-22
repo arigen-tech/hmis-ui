0ac286d1b7369bb420e2a11de7cfc2b4e9980b92
“HariomBhardwaj19” <hbstudent1997@gmail.com>
2025-12-22 11:01:08 +0530

    InvestigationMethodologyMaster ui fix

--- src/Views/Masters/InvestigationMethodologyMaster/index.js
+++ src/Views/Masters/InvestigationMethodologyMaster/index.js
@@ -169,9 +169,9 @@ const InvestigationMethodologyMaster = () => {
             <div className="row">
                 <div className="col-12 grid-margin stretch-card">
                     <div className="card form-card">
-                        <div className="card-header">
+                        <div className="card-header d-flex justify-content-between align-items-center">
                             <h4 className="card-title p-2">Investigation Methodology Master</h4>
-                            <div className="d-flex justify-content-between align-items-center mt-3">
+                            <div className="d-flex justify-content-between align-items-center">
                                 {!showForm && (
                                     <form className="d-inline-block searchform me-4" role="search">
                                         <div className="input-group searchinput">
