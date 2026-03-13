a39c93da07bce59847977678c6f8cfd27e14edd7
Ritu Kushwaha <reetukushwah50@gmail.com>
2026-03-13 14:55:54 +0530

    OpthMasSpectacleUse  binding complete

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -256,6 +256,7 @@ export const MAS_STERILISATION = `${MASTERS}/gynMasSterilisation`;
 export const MAS_STATION_PRESENTATION =`${MASTERS}/obMasStationPresenting`;
 export const MAS_OPTH_LENSTYPE =`${MASTERS}/opthMasLensType`;
 export const MAS_MENARCHE_AGE = `${MASTERS}/gynMasMenarcheAge`;
+export const MAS_OPTH_SPECTACLE_USE =`${MASTERS}/opthMasSpectacleUse`;
 export const MAS_EAR_CANAL = `${MASTERS}/entMasEarCanal`;
 export const MAS_ENT_RINNE = `${MASTERS}/entMasRinne`;
 export const MAS_OPTH_NEAR_VISION =`${MASTERS}/opthMasNearVision`;
